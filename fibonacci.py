874eb9a520fafdd0af8b928e74201c9cda4d1e7c
Omar Akoudad Muñoz <62266745+oakoudad1@users.noreply.github.com>
2020-03-17 19:08:05 +0100

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+n=int(input("\nIntroduzca un numero: "));
+a=0
+b=1
+z=0
+for i in range(n):
+	z=a+b
+	print(str(z))
+	a=b
+	b=z
+print("\nEstos son los primeros " + str(n) + " numeros de la sucesión de Fibonacci")
+	
