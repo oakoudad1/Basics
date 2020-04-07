70a1d39c18e802b4ee3ac86d4ea8c222523587c7
Omar Akoudad Muñoz <62266745+oakoudad1@users.noreply.github.com>
2020-04-07 16:20:15 +0200

    Add files via upload

---
+++ duracion.py
@@ -0,0 +1,10 @@
+hora = int(input("Hora de inicio (horas): "))
+minu = int(input("Minuto de inicio (minutos): "))
+dura = int(input("Duración del evento (minutos): "))
+
+minu +=dura
+dato = minu // 60
+minufinal = minu % 60
+horaFinal = dato + hora
+horaMostrada = horaFinal % 24
+print(str(horaMostrada),str(minufinal),sep=":")
